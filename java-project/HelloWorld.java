5a284e8eff679d557de6ea8aa806cc7ec0aea12c
Mujahid93 <md.mujahid079@gmail.com>
2025-03-06 20:31:36 +0530

    First commit of HelloWorld.java

---
+++ java-project/HelloWorld.java
@@ -0,0 +1,11 @@
+// This is a simple java program
+// Filename: HelloWorld.java
+
+class HelloWorld {
+   public static void main (Strings args[])
+   {
+      System.out.println ("Hello World");
+   }
+
+}
+
